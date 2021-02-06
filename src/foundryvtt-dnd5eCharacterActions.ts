ae262045518f024ff90bb3ba0ffeb76de8a343d8
akrigline <a.krigline@gmail.com>
2021-02-05 19:04:06 -0500

    refactor: :art: remove semaphore in favor of checking if the element already exists
    resolves #10 even better

--- src/foundryvtt-dnd5eCharacterActions.ts
+++ src/foundryvtt-dnd5eCharacterActions.ts
@@ -14,31 +14,11 @@ Handlebars.registerHelper(`${MODULE_ABBREV}-isEmpty`, (input: Object | Array<any
   return isObjectEmpty(input);
 });
 
-/**
- * Challenge: Want to be able to use this without a tab on some character sheets
- * Problems:
- * - If I attach this to the default 5e character sheet render hook, it'll be on all character sheets that extend that sheet.
- *
- * Challenge: Want to be able to use this without a sheet at all.
- *
- * Potential solutions:
- * if any sheet has called the 'add actions tab' function already, don't call it on render hook for the default sheet
- * - what is the order of hooks being fired? does custom come first, or second?
- * ---> the hooks fire with custom starting first
- *
- * Solution:
- * Keep a map of appIds that have called `addActionsTab` and return out of the function if that appId is calling it again
- * On Cleanup, remove the appId.
- */
-
+// deprecated
 const actionsActionsListRenderers = new Set();
 
 /**
- * Add the Actions Tab to Sheet HTML
- * This does not register any event listeners as I am hoping that this suggestion will be adopted:
- * https://gitlab.com/foundrynet/foundryvtt/-/issues/3998
- *
- * this should only be called if a sheet hasn't yet rendered the actions list themselves
+ * Add the Actions Tab to Sheet HTML. Returns early if the character-actions-dnd5e element already exists
  */
 async function addActionsTab(
   app: Application & {
@@ -47,8 +27,10 @@ async function addActionsTab(
   html,
   data: ActorSheet5eCharacterSheetData
 ) {
-  // check if what is rendering this is an Application
-  if (!!app.appId && actionsActionsListRenderers.has(app.appId)) {
+  const existingActionsList = $(html).find('.character-actions-dnd5e');
+
+  // check if what is rendering this is an Application and if our Actions List exists within it already
+  if (!!app.appId && !!existingActionsList.length) {
     return;
   }
 
@@ -63,7 +45,7 @@ async function addActionsTab(
   sheetBody.prepend(actionsTab);
 
   // add the list to the tab
-  const actionsTabHtml = $(await renderActionsList(app.object, app.appId));
+  const actionsTabHtml = $(await renderActionsList(app.object));
   actionsTab.append(actionsTabHtml);
 
   // listeners
@@ -73,26 +55,13 @@ async function addActionsTab(
   actionsTabHtml.find('.item .item-name.rollable h4').click((event) => app._onItemSummary(event));
 }
 
-function cleanupActionsTab(appId) {
-  log(false, 'cleanupActionsTab pre', {
-    actionsActionsListRenderers,
-    appId,
-  });
-  // add this appId to the list of renderers
-  actionsActionsListRenderers.delete(appId);
-  log(false, 'cleanupActionsTab post', {
-    actionsActionsListRenderers,
-  });
-}
-
-async function renderActionsList(actorData: Actor5eCharacter, appId) {
-  // check if what is rendering this is an Application
-  if (!!appId && actionsActionsListRenderers.has(appId)) {
-    return;
+async function renderActionsList(actorData: Actor5eCharacter, appId?: number) {
+  if (!!appId) {
+    actionsActionsListRenderers.add(appId); // deprecated
+    console.warn(
+      'providing the appId to ActionsList5e is deprecated with Character Actions List 2.0.0 and will be removed in a future update, check if your sheet module has an update'
+    );
   }
-
-  // add this appId to the list of renderers
-  actionsActionsListRenderers.add(appId);
 
   const data = await getActorActionsData(actorData);
 
@@ -123,114 +92,24 @@ Hooks.once('init', async function () {
   Hooks.call(`CharacterActions5eReady`);
 });
 
-/* ------------------------------------ */
-/* Setup module							*/
-/* ------------------------------------ */
-Hooks.once('setup', function () {
-  // Do anything after initialization but before
-  // ready
-});
-
-/* ------------------------------------ */
-/* When ready							*/
-/* ------------------------------------ */
-Hooks.once('ready', function () {
-  // Do anything once the module is ready
-});
-
-// Add any additional hooks if necessary
-
 // default sheet injection if this hasn't yet been injected
 Hooks.on('renderActorSheet5e', (app, html, data) => {
   log(false, 'default sheet open hook firing', {
     app,
     html,
     data,
-    actionsActionsListRenderers,
   });
 
-  // hacky way to make default sheet injection work
-  // danger for duplicating on other sheets using the api correctly?
-  Hooks.once('updateActor', (actor) => {
-    log(false, 'updateActor hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('updateOwnedItem', (actor) => {
-    log(false, 'updateOwnedItem hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('deleteOwnedItem', (actor) => {
-    log(false, 'updateOwnedItem hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('updateActiveEffect', (actor) => {
-    log(false, 'updateOwnedItem hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('deleteActiveEffect', (actor) => {
-    log(false, 'updateOwnedItem hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('createActiveEffect', (actor) => {
-    log(false, 'updateOwnedItem hook firing', {
-      apps: actor.apps,
-      actionsActionsListRenderers,
-    });
-
-    Object.keys(actor.apps).forEach((appId) => {
-      cleanupActionsTab(Number(appId));
-    });
-  });
-
-  Hooks.once('closeActorSheet5e', (app, html, data) => {
-    log(false, 'default sheet close hook firing', {
-      app,
-      html,
-      data,
-      actionsActionsListRenderers,
-    });
-
-    cleanupActionsTab(app.appId);
-  });
-
-  // this is awful... need to find a better way to handle cleanup
-
+  // Deprecated
   if (actionsActionsListRenderers.has(app.appId)) {
+    return;
+  }
+
+  const actionsList = $(html).find('.character-actions-dnd5e');
+
+  log(false, 'actionsListExists', { actionsListExists: actionsList.length });
+
+  if (!!actionsList.length) {
     return;
   }
 

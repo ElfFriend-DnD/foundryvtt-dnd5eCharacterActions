e5a4a0a8a60d5de2cab319cc212706b29d00adef
akrigline <a.krigline@gmail.com>
2022-02-05 13:07:21 -0500

    refactor: greatly improve sorting logic

--- src/module/getActorActionsData.ts
+++ src/module/getActorActionsData.ts
@@ -1,10 +1,39 @@
-import { getActivationType, isItemInActionList, log } from './helpers';
+import { getActivationType, getGame, isItemInActionList, log } from './helpers';
+
+enum ItemTypeSortValues {
+  weapon = 1,
+  equipment = 2,
+  feat = 3,
+  spell = 4,
+  consumable = 5,
+  tool = 6,
+  backpack = 7,
+  class = 8,
+  loot = 9,
+}
 
 export function getActorActionsData(actor: Actor5e) {
-  const filteredItems = actor.items.filter(isItemInActionList).sort((a, b) => {
-    log(false, 'sorting', a.name, b.name, a.data.sort || 0, b.data.sort || 0);
-    return (a.data.sort || 0) - (b.data.sort || 0);
-  });
+  const filteredItems = actor.items
+    .filter(isItemInActionList)
+    .sort((a, b) => {
+      if (a.data.type !== b.data.type) {
+        return ItemTypeSortValues[a.data.type] - ItemTypeSortValues[b.data.type];
+      }
+
+      if (a.data.type === 'spell' && b.data.type === 'spell') {
+        return a.data.data.level - b.data.data.level;
+      }
+
+      return (a.data.sort || 0) - (b.data.sort || 0);
+    })
+    .map((item) => {
+      if (item.labels) {
+        //@ts-expect-error
+        item.labels.type = getGame().i18n.localize(`DND5E.ItemType${item.type.titleCase()}`);
+      }
+
+      return item;
+    });
 
   const actionsData: Record<
     'action' | 'bonus' | 'crew' | 'lair' | 'legendary' | 'reaction' | 'other',
